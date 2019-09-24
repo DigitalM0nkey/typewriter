fcd6226ead782f05008dfa5d7f9bf114409a36b2
Terrence Kuiper <github@terrencekuiper.com>
2019-09-24 14:23:24 -0700

    Initial commit of typewriter.js pair programming with @lloydGerry with help from @smesworld

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+//string to use
+const sentence = "hello there from lighthouse labs";
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    return process.stdout.write(sentence[i]);
+    setTimeout
+  }, i * 1000);
+}
